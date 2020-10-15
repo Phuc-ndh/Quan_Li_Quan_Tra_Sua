1d503cb1419dbb1e79589e10ede36f69f640a233
Vũ Quân <72614272+penwin1408@users.noreply.github.com>
2020-10-16 02:51:20 +0700

    Project

---
+++ QuanLyQuanTraSua/DAL/DBConnect.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    class DBConnect
+    {
+        private SqlConnection connection;
+        public DBConnect()
+        {
+            connection = new SqlConnection(@"Data Source=DESKTOP-JSEKHS1;Initial Catalog=QUANLYTRASUA;Integrated Security=True");
+        }
+        public DataTable ExecuteQuery(string query)
+        {
+            Open();
+            SqlCommand command = new SqlCommand(query, connection);
+            SqlDataAdapter da = new SqlDataAdapter(command);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            Close();
+            return dt;
+        }
+        public void Open()
+        {
+            if (connection.State == ConnectionState.Closed)
+                connection.Open();
+        }
+        public void Close()
+        {
+            if (connection.State == ConnectionState.Open)
+                connection.Close();
+        }
+
+    }
+}
---
+++ QuanLyQuanTraSua/GUI/frmLogin.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+using BUS;
+
+namespace QuanLyQuanTraSua
+{
+    public partial class frmLogin : Form
+    {
+        AccountBUS accountBus;
+        public frmLogin()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (accountBus.Login(txtID.Text, txtPassword.Text))
+            {
+                this.Hide();
+                frmMain form = new frmMain();
+                form.ShowDialog();
+                this.Show();
+            }
+            else
+            {
+                MessageBox.Show("Sai mật khẩu hoặc tài khoản");
+                txtPassword.Text = "";
+                txtPassword.Focus();
+            }
+        }
+
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+
+            if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                btnLogin_Click(sender, e);
+            }
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
