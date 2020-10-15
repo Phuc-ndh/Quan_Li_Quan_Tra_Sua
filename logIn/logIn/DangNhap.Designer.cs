caa4d63691ceb101ffe78dea677a959bc797b9fc
Vũ Quân <72614272+penwin1408@users.noreply.github.com>
2020-10-15 22:32:59 +0700

    Add files via upload

---
+++ logIn/logIn/DangNhap.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace logIn
+{
+    partial class logIn
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblID = new System.Windows.Forms.Label();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblID.Location = new System.Drawing.Point(40, 48);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(27, 24);
+            this.lblID.TabIndex = 0;
+            this.lblID.Text = "ID";
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lblPassword.Location = new System.Drawing.Point(40, 110);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(92, 24);
+            this.lblPassword.TabIndex = 1;
+            this.lblPassword.Text = "Password";
+            // 
+            // txtID
+            // 
+            this.txtID.Font = new System.Drawing.Font("Imprint MT Shadow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtID.Location = new System.Drawing.Point(166, 43);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(266, 30);
+            this.txtID.TabIndex = 0;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Font = new System.Drawing.Font("Imprint MT Shadow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPassword.Location = new System.Drawing.Point(166, 105);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(266, 30);
+            this.txtPassword.TabIndex = 1;
+            this.txtPassword.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtPassword_KeyPress);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCancel.Location = new System.Drawing.Point(166, 173);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(120, 40);
+            this.btnCancel.TabIndex = 3;
+            this.btnCancel.Text = "Thoát";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnLogin.Location = new System.Drawing.Point(312, 173);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(120, 40);
+            this.btnLogin.TabIndex = 2;
+            this.btnLogin.Text = "Đăng Nhập";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            this.btnLogin.Click += new System.EventHandler(this.btnLogin_Click);
+            // 
+            // logIn
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(495, 262);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblPassword);
+            this.Controls.Add(this.lblID);
+            this.Name = "logIn";
+            this.Text = "DangNhap";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnLogin;
+    }
+}
+
---
+++ logIn/logIn/DangNhap.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+using System.Security.Cryptography;
+
+namespace logIn
+{
+    public partial class logIn : Form
+    {
+        public logIn()
+        {
+            InitializeComponent();
+        }
+
+        // lay chuoi connect sql
+        private string connectString = @"Data Source=DESKTOP-JSEKHS1;Initial Catalog=QUANLYTRASUA;Integrated Security=True";
+
+        // ham lay chuoi connect
+        public string getConnectString()
+        {
+            string connect_str = connectString;
+            return connect_str;
+        }
+
+        SqlConnection connection;
+
+
+        // ham ma hoa pass MD5
+        public StringBuilder getHashMD5(string pass)
+        {
+            MD5 hash = MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(pass);
+            byte[] outputBytes = hash.ComputeHash(inputBytes);
+            StringBuilder hash_str = new StringBuilder();
+            for (int i = 0; i < outputBytes.Length; i++)
+            {
+                hash_str.Append(outputBytes[i].ToString("x2"));
+            }
+            return hash_str;
+        }
+
+        private static string USER_ID = "";
+
+        // lay user va password nhap tu textbox
+        private string getId(string user, string pass)
+        {
+            string id = "";
+            try
+            {
+                connection = new SqlConnection(connectString);
+                connection.Open();
+                SqlCommand cmd = new SqlCommand("SELECT* FROM USERS WHERE ID ='" + user + "'AND PASS = '" + getHashMD5(pass) + "'", connection);
+                SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+                DataTable table = new DataTable();
+                adapter.Fill(table);
+                if (table != null)
+                {
+                    foreach (DataRow row in table.Rows)
+                    {
+                        id = row["ID"].ToString();
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Không kết nối đến server được !");
+            }
+            finally
+            {
+                connection.Close();
+            }
+            return id;
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            USER_ID = getId(txtID.Text, txtPassword.Text);
+            if (USER_ID != "")
+            {
+                // goi ham form main ra
+                MessageBox.Show("Form Main sẽ hiện ra");
+            }
+            else
+            {
+                MessageBox.Show("Sai mật khẩu hoặc tài khoảng");
+                txtPassword.Text = "";
+                txtPassword.Focus();
+            }
+        }
+
+        private void txtPassword_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            
+             if (e.KeyChar == Convert.ToChar(Keys.Enter))
+             {
+                    btnLogin_Click(sender, e);
+             }          
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
