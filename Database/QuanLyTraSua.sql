e45011fa1e069590102c4c1229efe5a2d1bbcaea
Vũ Quân <72614272+penwin1408@users.noreply.github.com>
2020-10-16 20:03:46 +0700

    Add DTO

--- Database/QuanLyTraSua.sql
+++ Database/QuanLyTraSua.sql
@@ -1,59 +1,56 @@
-﻿--create database QuanLyTraSua
+﻿create database QuanLyTraSua
 go
 use QuanLyTraSua
 
 
 create table DrinkCategory
 (
-	id int identity not null, constraint PK_idDrinkCategory primary key (id),
+	idCategory int identity not null, constraint PK_idCategory primary key (idCategory),
 	Name varchar(40)
 )
 
 create table Drink
 (
-	id int identity not null, constraint PK_idDrink primary key (id),
+	idDrink int identity not null, constraint PK_idDrink primary key (idDrink),
 	Name varchar(40),
 	Price int,
 	idCategory int
 )
 alter table Drink add
-constraint FK_Drink_idCategory foreign key (idCategory) references DrinkCategory(id)
+constraint FK_Drink_idCategory foreign key (idCategory) references DrinkCategory(idCategory)
 
 create table _Table
 (
-	id int identity not null, constraint PK_idTable primary key (id),
+	idTable int identity not null, constraint PK_idTable primary key (idTable),
 	Name nvarchar(40),
-	Status nvarchar(20) default 0 -- 0: trống , 1: đã có người
+	Status nvarchar(20) default 0 --0: trống , 1: đã có người
 )
 
 create table Account
 (
 	Username nvarchar(40) not null, constraint PK_Username primary key (Username),
-	RealName nvarchar(40),
-	Password nvarchar(40) default 1,
-	Type int default 0, -- 1 = admin
-	Email nvarchar(40),
-	PhoneNumber nvarchar(40)
+	DisplayName nvarchar(100),
+	Password nvarchar(40) default 0
 )
 
 create table Bill
 (
-	id int identity not null, constraint PK_idBill primary key (id),
+	idBill int identity not null, constraint PK_idBill primary key (idBill),
 	Date smalldatetime,
 	TotalPrice int,
 	idTable int
 )
 alter table Bill add
-constraint FK_Bill_idTable foreign key (idTable) references _Table(id)
+constraint FK_Bill_idTable foreign key (idTable) references _Table(idTable)
 
 
 create table BillInfo
 (
 	idBill int not null,
 	idDrink int,
-	Amount int,
+	Quantity int,
 	constraint PK_BillInfo primary key (idBill, idDrink)	
 )
 alter table BillInfo add
-constraint FK_BillInfo_idBill foreign key (idBill) references Bill(id),
-constraint FK_BillInfo_idDrink foreign key (idDrink) references Drink(id)
+constraint FK_BillInfo_idBill foreign key (idBill) references Bill(idBill),
+constraint FK_BillInfo_idDrink foreign key (idDrink) references Drink(idDrink)
