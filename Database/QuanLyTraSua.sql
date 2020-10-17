9b9b9b2a74826c40a6a0c249aadcfa38b796a944
Vũ Quân <72614272+penwin1408@users.noreply.github.com>
2020-10-17 23:50:04 +0700

    Update QuanLyTraSua.sql

--- Database/QuanLyTraSua.sql
+++ Database/QuanLyTraSua.sql
@@ -31,7 +31,7 @@ create table Account
 	Username nvarchar(40) not null, constraint PK_Username primary key (Username),
 	DisplayName nvarchar(100),
 	Password nvarchar(40) default 0,
-	Access nvarchar(10)
+	Type int default 0 --1: admin
 )
 
 create table Bill
