fb93e8d476598d52cd950ca80d3bcdda1ef3d18f
Phuc-ndh <phucuuit@gmailc.om>
2020-10-18 23:29:01 +0700

    update gui center

--- QuanLyQuanTraSua/GUI/frmMain.Designer.cs
+++ QuanLyQuanTraSua/GUI/frmMain.Designer.cs
@@ -33,6 +33,12 @@
             System.Windows.Forms.ListViewItem listViewItem3 = new System.Windows.Forms.ListViewItem("Giá");
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
             this.panel1 = new System.Windows.Forms.Panel();
+            this.label7 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.txtThoiGian = new System.Windows.Forms.TextBox();
+            this.txtTenNV = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             this.panel2 = new System.Windows.Forms.Panel();
             this.btnDangXuat = new System.Windows.Forms.Button();
             this.btnAdmin = new System.Windows.Forms.Button();
@@ -59,15 +65,10 @@
             this.tabPage5 = new System.Windows.Forms.TabPage();
             this.tabPage6 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.label7 = new System.Windows.Forms.Label();
-            this.panel3 = new System.Windows.Forms.Panel();
-            this.txtTenNV = new System.Windows.Forms.TextBox();
-            this.label5 = new System.Windows.Forms.Label();
-            this.label6 = new System.Windows.Forms.Label();
-            this.txtThoiGian = new System.Windows.Forms.TextBox();
-            this.btnLamMoi = new System.Windows.Forms.Button();
             this.flpDSOrder = new System.Windows.Forms.FlowLayoutPanel();
+            this.btnLamMoi = new System.Windows.Forms.Button();
             this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.panel2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
             this.splitContainer1.Panel1.SuspendLayout();
@@ -79,7 +80,6 @@
             this.tabPage1.SuspendLayout();
             this.tabControl2.SuspendLayout();
             this.tabPage2.SuspendLayout();
-            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
@@ -93,6 +93,76 @@
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(1264, 67);
             this.panel1.TabIndex = 0;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(139, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(701, 24);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "CHÚC BẠN CÓ MỘT NGÀY VUI VẺ  -  HÃY LUÔN TƯƠI CƯỜI VỚI KHÁCH HÀNG";
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.txtThoiGian);
+            this.panel3.Controls.Add(this.txtTenNV);
+            this.panel3.Controls.Add(this.label5);
+            this.panel3.Controls.Add(this.label6);
+            this.panel3.Location = new System.Drawing.Point(891, 1);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(373, 64);
+            this.panel3.TabIndex = 5;
+            // 
+            // txtThoiGian
+            // 
+            this.txtThoiGian.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtThoiGian.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtThoiGian.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.29412F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtThoiGian.ForeColor = System.Drawing.Color.White;
+            this.txtThoiGian.Location = new System.Drawing.Point(113, 37);
+            this.txtThoiGian.Name = "txtThoiGian";
+            this.txtThoiGian.ReadOnly = true;
+            this.txtThoiGian.Size = new System.Drawing.Size(224, 19);
+            this.txtThoiGian.TabIndex = 1;
+            this.txtThoiGian.Text = "thoigian";
+            // 
+            // txtTenNV
+            // 
+            this.txtTenNV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
+            this.txtTenNV.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtTenNV.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.29412F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenNV.ForeColor = System.Drawing.Color.White;
+            this.txtTenNV.Location = new System.Drawing.Point(113, 7);
+            this.txtTenNV.Name = "txtTenNV";
+            this.txtTenNV.ReadOnly = true;
+            this.txtTenNV.Size = new System.Drawing.Size(224, 19);
+            this.txtTenNV.TabIndex = 1;
+            this.txtTenNV.Text = "nv1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.29412F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(13, 7);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(88, 20);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Nhân viên:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.29412F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(13, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(82, 20);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Thời gian:";
             // 
             // panel2
             // 
@@ -460,75 +530,12 @@
             this.tabPage2.TabIndex = 1;
             this.tabPage2.Text = "Danh sách order";
             // 
-            // label7
+            // flpDSOrder
             // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F);
-            this.label7.ForeColor = System.Drawing.Color.White;
-            this.label7.Location = new System.Drawing.Point(139, 20);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(701, 24);
-            this.label7.TabIndex = 4;
-            this.label7.Text = "CHÚC BẠN CÓ MỘT NGÀY VUI VẺ  -  HÃY LUÔN TƯƠI CƯỜI VỚI KHÁCH HÀNG";
-            // 
-            // panel3
-            // 
-            this.panel3.Controls.Add(this.txtThoiGian);
-            this.panel3.Controls.Add(this.txtTenNV);
-            this.panel3.Controls.Add(this.label5);
-            this.panel3.Controls.Add(this.label6);
-            this.panel3.Location = new System.Drawing.Point(891, 1);
-            this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(373, 64);
-            this.panel3.TabIndex = 5;
-            // 
-            // txtTenNV
-            // 
-            this.txtTenNV.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtTenNV.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtTenNV.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.29412F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtTenNV.ForeColor = System.Drawing.Color.White;
-            this.txtTenNV.Location = new System.Drawing.Point(113, 7);
-            this.txtTenNV.Name = "txtTenNV";
-            this.txtTenNV.ReadOnly = true;
-            this.txtTenNV.Size = new System.Drawing.Size(224, 19);
-            this.txtTenNV.TabIndex = 1;
-            this.txtTenNV.Text = "nv1";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.29412F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.ForeColor = System.Drawing.Color.White;
-            this.label5.Location = new System.Drawing.Point(13, 7);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(88, 20);
-            this.label5.TabIndex = 0;
-            this.label5.Text = "Nhân viên:";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.29412F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.ForeColor = System.Drawing.Color.White;
-            this.label6.Location = new System.Drawing.Point(13, 37);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(82, 20);
-            this.label6.TabIndex = 0;
-            this.label6.Text = "Thời gian:";
-            // 
-            // txtThoiGian
-            // 
-            this.txtThoiGian.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(64)))), ((int)(((byte)(64)))));
-            this.txtThoiGian.BorderStyle = System.Windows.Forms.BorderStyle.None;
-            this.txtThoiGian.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.29412F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtThoiGian.ForeColor = System.Drawing.Color.White;
-            this.txtThoiGian.Location = new System.Drawing.Point(113, 37);
-            this.txtThoiGian.Name = "txtThoiGian";
-            this.txtThoiGian.ReadOnly = true;
-            this.txtThoiGian.Size = new System.Drawing.Size(224, 19);
-            this.txtThoiGian.TabIndex = 1;
-            this.txtThoiGian.Text = "thoigian";
+            this.flpDSOrder.Location = new System.Drawing.Point(6, 39);
+            this.flpDSOrder.Name = "flpDSOrder";
+            this.flpDSOrder.Size = new System.Drawing.Size(504, 523);
+            this.flpDSOrder.TabIndex = 5;
             // 
             // btnLamMoi
             // 
@@ -548,13 +555,6 @@
             this.btnLamMoi.Text = "Làm mới";
             this.btnLamMoi.UseVisualStyleBackColor = false;
             // 
-            // flpDSOrder
-            // 
-            this.flpDSOrder.Location = new System.Drawing.Point(6, 39);
-            this.flpDSOrder.Name = "flpDSOrder";
-            this.flpDSOrder.Size = new System.Drawing.Size(504, 523);
-            this.flpDSOrder.TabIndex = 5;
-            // 
             // frmMain
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -567,9 +567,12 @@
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Quản lý quán trà sữa";
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.splitContainer1.Panel1.ResumeLayout(false);
             this.splitContainer1.Panel1.PerformLayout();
@@ -583,8 +586,6 @@
             this.tabPage1.ResumeLayout(false);
             this.tabControl2.ResumeLayout(false);
             this.tabPage2.ResumeLayout(false);
-            this.panel3.ResumeLayout(false);
-            this.panel3.PerformLayout();
             this.ResumeLayout(false);
 
         }
