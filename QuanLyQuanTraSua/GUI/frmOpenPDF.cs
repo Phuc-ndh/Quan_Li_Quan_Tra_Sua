126d7a5d714bc055c44b7417bb6613db97815c85
Phuc-ndh <phucuuit@gmail.com>
2020-12-24 23:43:16 +0700

    open receipt.pdf in form

---
+++ QuanLyQuanTraSua/GUI/frmOpenPDF.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLyQuanTraSua
+{
+    public partial class frmOpenPDF : Form
+    {
+        public frmOpenPDF()
+        {
+            InitializeComponent();
+        }
+        public frmOpenPDF(string path)
+        {
+            InitializeComponent();
+            webBrowser1.Navigate(@path);
+        }
+    }
+}
