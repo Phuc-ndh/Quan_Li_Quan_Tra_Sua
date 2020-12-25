814ca4eb6adcca6ac6a72f1727f478c40142b2e8
Vũ Quân <72614272+penwin1408@users.noreply.github.com>
2020-12-26 03:21:36 +0700

    Thêm class ImageHelper

---
+++ QuanLyQuanTraSua/GUI/ImageHelper.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuanLyQuanTraSua
+{
+    public static class ImageHelper
+    {
+        public static byte[] ImageToByteArray(Image img)
+        {
+            using (MemoryStream ms = new MemoryStream())
+            {
+                img.Save(ms, img.RawFormat);
+                return ms.ToArray();
+            }
+        }
+
+        public static Image ByteArrayToImage(byte[] bytes)
+        {
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                Image img = Image.FromStream(ms);
+                return img;
+            }
+        }
+    }
+}
